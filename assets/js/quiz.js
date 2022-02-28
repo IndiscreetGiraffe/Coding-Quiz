9b508500f7fbc9b6ff254bbb7a8781a57879037b
Kyleah Justice <kikij432@gmail.com>
2022-02-28 16:46:40 -0700

    tthe buttons should be done. I love google,because I can't ever remember how I'm supposed to lay this out

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -147,88 +147,41 @@ function nextQuestion() {
                 timeLeft = timeLeft - 2;
         }
     })
-    
+    btn2.addEventListener("click", function (event) {
+        if (event.target.innerText === righttAnswers[index]) {
+            score +
+                rightResultsEl.setAttribute("style", "display: block;");
+        }
+        else {
+            score -
+                wrongResultsEl.setAttribute("style", "display: block;");
+                timeLeft = timeLeft - 3;
+        }
+    })
 
+    btn3.addEventListener("click", function (event) {
+        if (event.target.innerText === rightAnswers[index]) {
+            score +
+                rightResultsEl.setAttribute("style", "display: block;");
+        }
+        else {
+            score -
+                wrongResultsEl.setAttribute("style", "display: block;");
+                timeLeft = timeLeft - 3;
+        }
+    })
 
+    btn4.addEventListener("click", function (event) {
+        if (event.target.innerText === rightAnswers[index]) {
+            score +
+                rightResultsEl.setAttribute("style", "display: block;");
+        }
+        else {
+            score -
+                wrongResultsEl.setAttribute("style", "display: block;");
+                timeLeft = timeLeft - 3;
 
+        }
+    })
 
-
-
-
-
-
-
-/*This is the old js stuff, I realize now why it wasn't working. I'm going to use the var method to get this working(fucntion(){
-    function buildQuiz(){
-
-        const output = [];
     
-        myQuestions.forEach(
-            (currentQuestion, questionNumber) => {
-                
-                const answers = [];
-    
-                for(letter in currentQuestion.answers){
-    
-                    answers.push(
-                        `<label>
-                        <input type="radio" name="question${questionNumber}" value="${number}
-                        ${number} :
-                        ${currentQuestion.answers[number]}
-                        </label>`
-                    );
-                }
-    
-                outpush.push(
-                    `<div class="question"> ${currentQuestion.question} </div>
-                    <div class="answers"> ${answers.join('')} </div>`
-                );
-            }
-        );
-    
-        quizContainer.innerHTML = output.join('');
-    };
-    
-    function showResults(){
-        const answerContainers = quizContainer.quereySelectorAll('.answers');
-    
-        let numCorrect = 0;
-    
-        myQuestions.forEach( (currentQuestion, questionNumber) => {
-            
-            const answerContainer = answerContainers[qeustionNumber];
-            const selector = `input[name=question${questionNumber}]:checked`;
-            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-    
-            if(userAnswer === currentQuestion.correctAnswer){
-    
-                numCorrect++;
-    
-                answerContainers[questionNumber].style.color = 'purple';
-            }
-    
-            else{
-    
-                answerContainers[questionNumber].style.color = 'red';
-            }
-        });
-    
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
-    const submitButton = document.getElementById('submit');
-    const myQuestions = [
-        {
-            question: "Commonly used data types do NOT include:"
-            answers: {
-                1. "Strings",
-                2. "Booleans",
-                3. "Alerts",
-                4. "Numbers"
-            },
-            correctAnswer: "3."
-        };
-    ];
-    
-    submitButton.addEventListener('click',showResults);
-    })();
-    console.log(click, hello);
